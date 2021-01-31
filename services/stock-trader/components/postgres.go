5f16fecdc65721946a39fd919353b59fd4285361
Vickey Shrestha <logontovickey@hotmail.com>
2021-01-31 15:33:02 -0600

    - building project structure

---
+++ services/stock-trader/components/postgres.go
@@ -0,0 +1,4 @@
+package stocktrader
+
+type postgresClient struct {
+}
---
+++ services/stock-trader/database/stock-trader.sql
@@ -0,0 +1,11 @@
+-- Creates table actions if doesn't exist
+CREATE TABLE IF NOT EXISTS stocktrader.actions (
+    uid character varying NOT null,
+    symbol character varying (50),
+    company name character varying (50),
+    action_type character varying (50),
+    current_value double precision,
+    added_timestamp timestamp DEFAULT CURRENT_TIMESTAMP ,
+    is_deleted boolean DEFAULT false,
+    CONSTRAINT actions_pkey PRIMARY KEY (uid)
+);
