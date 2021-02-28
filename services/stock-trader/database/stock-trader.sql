c35cf737fa5bd791ee375e3e59126913a411466f
Vickey Shrestha <logontovickey@hotmail.com>
2021-02-27 19:17:12 -0600

    - compnay name was fixed

--- services/stock-trader/database/stock-trader.sql
+++ services/stock-trader/database/stock-trader.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS stocktrader.actions (
     uid character varying NOT null,
     symbol character varying (50),
-    company name character varying (50),
+    company_name character varying (50),
     action_type character varying (50),
     current_value double precision,
     added_timestamp timestamp DEFAULT CURRENT_TIMESTAMP ,
